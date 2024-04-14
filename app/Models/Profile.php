bef9994c6d5aa344a8ad48ab4ff303b3137f4707
Vincent Cavanna <vcavanna@wordonfire.org>
2024-04-14 14:53:12 -0500

    Added Profile model

---
+++ app/Models/Profile.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'gender',
+        'email',
+        'linkedin',
+        'studies',
+        'interests',
+        'graduation_year',
+    ];
+}
---
+++ database/migrations/2024_04_13_030612_create_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->enum('gender', ['male', 'female']);
+            $table->string('email')->unique();
+            $table->enum('prefix', ['Mr.', 'Ms.', 'Dr.', 'Prof.'])->nullable();
+            $table->string('linkedin')->nullable();
+            $table->string('studies');
+            $table->string('interests')->nullable();
+            $table->year('graduation_year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
