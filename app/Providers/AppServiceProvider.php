db8d64b2b50684d5265c4c85f2b8cbe375c53c45
Vincent Cavanna <vcavanna@wordonfire.org>
2024-04-27 12:07:19 -0500

    Set a default user timezone to Chicago

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Tables\Columns\TextColumn;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        DateTimePicker::configureUsing(fn(DateTimePicker $component) => $component->timezone(config('app.user_timezone')));
+        TextColumn::configureUsing(fn(TextColumn $column) => $column->timezone(config('app.user_timezone')));
     }
 }
