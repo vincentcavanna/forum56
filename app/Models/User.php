470f0fae0c0327080be2edb9c6b8f7cfa232ed2a
Vincent Cavanna <vcavanna@wordonfire.org>
2024-04-28 15:52:23 -0500

    Fixed canAccessPanel

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -43,8 +44,8 @@ class User extends Authenticatable implements FilamentUser
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-    
-    public function canAccessFilament(): bool
+
+    public function canAccessPanel(Panel $panel): bool
     {
         return true;
     }
